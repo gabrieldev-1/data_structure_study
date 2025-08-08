5e8c8f42ecbda0ca6c5f1da4e9b4b19c5eb9422b
Gabriel dos Santos <developergabrieldossantos@gmail.com>
2025-08-08 10:30:46 -0300

    /arrays exercise 'Find Duplicated'

---
+++ arrays/exercises/FindDuplicated.java
@@ -0,0 +1,24 @@
+package arrays.exercises;
+
+public class FindDuplicated {
+    public int value;
+
+    public int find(int array[]) {
+        for(int i = 0; i < array.length; i++) {
+            for(int j = 0; j < array.length; j++) {
+                if(array[i] == array[j] && i != j) {
+                    value = array[i];
+
+                }
+            }
+        }
+
+        return value;
+    }
+
+    public static void main(String[] args) {
+        int array[] = {1, 2, 10, 20, 3, 6, 7, 8, 9, 2};
+        FindDuplicated search = new FindDuplicated();
+        System.out.println(search.find(array));
+    }
+}
