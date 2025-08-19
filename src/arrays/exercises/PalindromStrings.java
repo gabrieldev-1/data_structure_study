8fbf07697b5bc62e4d3c1e84da2fdbfadce2b4e8
Gabriel dos Santos <developergabrieldossantos@gmail.com>
2025-08-19 16:13:03 -0300

    /arrays exercise 'PalindromStrings'

---
+++ src/arrays/exercises/PalindromStrings.java
@@ -0,0 +1,31 @@
+package arrays.exercises;
+
+public class PalindromStrings {
+    public boolean isPalindrome(String word) {
+        char[] strArr = word.toCharArray(); // transforma a string em um array de caracteres
+        int left = 0;
+        int right = strArr.length - 1;
+
+
+        while(left < right) {
+
+            if(strArr[left] != strArr[right]) {
+                return false;
+
+            }
+            left++;
+            right--;
+
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+        PalindromStrings solution = new PalindromStrings();
+        boolean result = solution.isPalindrome("radar");
+
+        System.out.println(result);
+    }
+}
