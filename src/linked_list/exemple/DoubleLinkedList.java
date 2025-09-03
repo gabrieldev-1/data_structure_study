08f7f4a18e1faf494e8b22aa1991cda9aebc7843
Gabriel dos Santos <developergabrieldossantos@gmail.com>
2025-09-03 17:35:23 -0300

    /linked_list exemple of double linked list(method add final)

--- src/linked_list/exemple/DoubleLinkedList.java
+++ src/linked_list/exemple/DoubleLinkedList.java
@@ -45,23 +45,75 @@ public class DoubleLinkedList {
         }
     }
 
+    public void deleteFront() {
+        if(head == null) {
+            System.out.println("A lista esta vazia.");
+            return;
+
+        }
+
+        if (head.next == null) {
+            head = null;
+            tail = null;
+
+        } else {
+            head = head.next;
+            head.previous = null;
+
+        }
+    
+    }
+
+    public void deleteFinal() {
+        if(head == null) {
+            System.out.println("A lista esta vazia.");
+            return;
+        }
+
+        if(head.next == null) {
+            head = null;
+            tail = null;
+
+        } else {
+            tail = tail.previous;
+            tail.next = null;
+        }
+    }
+
     public void printList() {
         Node curNode = head;
+        System.out.println("-----------------------");
 
         while(curNode != null) {
             System.out.println(" -> " + curNode.data);
             curNode = curNode.next;
+
         }
+
+
     }
 
     public static void main(String[] args) {
         DoubleLinkedList list = new DoubleLinkedList();
 
-        list.addFront(10);
-        list.addFront(20);
-        list.addFront(30);
+        list.addFinal(10);
+        list.addFinal(20);
+        list.addFinal(30);
         list.addFinal(40);
         list.addFinal(50);
+
+        list.addFront(5);
+        list.addFront(2);
+
+        list.printList();
+
+        list.deleteFinal();
+        list.deleteFinal();
+        
+        list.printList();
+
+        list.deleteFront();
+        list.deleteFront();
 
         list.printList();
     }
